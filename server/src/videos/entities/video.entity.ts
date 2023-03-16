cc1b683d51acad04c817f5fe30b9c646def655c8
Nnamani John <nnamanijohn123@gmail.com>
2023-03-16 03:27:21 +0100

    create video endpoint and Video schema

---
+++ server/src/videos/entities/video.entity.ts
@@ -0,0 +1,22 @@
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {HydratedDocument} from "mongoose";
+import * as mongoose from "mongoose";
+import {User} from "../../users/entities/user.entity";
+
+export type VideoDocument = HydratedDocument<Video>
+
+@Schema()
+export class Video {
+    @Prop()
+    title: string;
+    @Prop()
+    video: string;
+    @Prop()
+    coverImage: string;
+    @Prop({default: Date.now()})
+    uploadAt: Date;
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'User'})
+    createdBy: User
+}
+
+export const VideoSchema = SchemaFactory.createForClass(Video);
