a810c2f4990f4832a9f07dd758a0fb72fd1b56b4
Nnamani John <nnamanijohn123@gmail.com>
2023-03-16 03:43:13 +0100

    import and inject user and video models to thier service

--- server/src/videos/videos.module.ts
+++ server/src/videos/videos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VideosService } from './videos.service';
 import { VideosController } from './videos.controller';
+import {MongooseModule} from "@nestjs/mongoose";
+import {Video, VideoSchema} from "./entities/video.entity";
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }])],
   controllers: [VideosController],
   providers: [VideosService]
 })
--- server/src/videos/videos.service.ts
+++ server/src/videos/videos.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
+import {InjectModel} from "@nestjs/mongoose";
+import {Video, VideoDocument} from "./entities/video.entity";
+import {Model} from "mongoose";
 
 @Injectable()
 export class VideosService {
+  constructor(@InjectModel(Video.name) private videoModel: Model<VideoDocument>) {
+  }
+
   create(createVideoDto: CreateVideoDto) {
     return 'This action adds a new video';
   }
